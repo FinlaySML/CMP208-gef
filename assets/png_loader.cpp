48b050b0c5be601ffe1c4e2ab4ce9551226671b3
2100719 <2100719@uad.ac.uk>
2023-03-06 11:45:19 +0000

    Revert "temp"
    This reverts commit c541eaa69b4fa73917fcb978eca686a4f4b32cee.

--- assets/png_loader.cpp
+++ assets/png_loader.cpp
@@ -7,8 +7,6 @@
 //#include <pngstruct.h>
 #include <stdlib.h>
 #include <cstring>
-#include <system/debug_log.h>
-#include <vector>
 
 typedef struct
 {
@@ -50,162 +48,193 @@ namespace gef
     {
         File* png_file   = gef::File::Create();
 
-        if(!png_file->Open(filename)){
-            gef::DebugOut("Could not open file: %s", filename);
-            return;
-        }
-        
-        Int32 file_size;
-        if(!png_file->GetSize(file_size)) {
-            gef::DebugOut("Could not get file size: %s", filename);
-            return;
-        }
-        std::vector<UInt8> buffer{ (size_t)file_size };
-        Int32 bytes_read;
-
-        if(!png_file->Read(buffer.data(), file_size, bytes_read)){
-            gef::DebugOut("Could not read file data: %s", filename);
-            return;
-        }
-        if(!png_file->Close()){
-            gef::DebugOut("Could not close file: %s", filename);
-            return;
-        }
-        png_structp png_ptr;
-        png_infop info_ptr;
-        UInt32 sig_read = 0;
-        png_uint_32 width = 0;
-        png_uint_32 height = 0;
-        int bitDepth = 0;
-        int colorType = -1;
-
-        /* Set error handling if you are
-            * using the setjmp/longjmp method
-            * (this is the normal method of
-            * doing things with libpng).
-            * REQUIRED unless you  set up
-            * your own error handlers in
-            * the png_create_read_struct()
-            * earlier.
-            */
-        if (setjmp(png_jmpbuf(png_ptr)))
+        bool success = png_file->Open(filename);
+        if(success)
         {
-            /* Free all of the memory associated
-                * with the png_ptr and info_ptr */
-            png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
-            return;
-        }
-
-
-        /* Create and initialize the png_struct
-            * with the desired error handler
-            * functions.  If you want to use the
-            * default stderr and longjump method,
-            * you can supply NULL for the last
-            * three parameters.  We also supply the
-            * the compiler header file version, so
-            * that we know if the application
-            * was compiled with a compatible version
-            * of the library.  REQUIRED
-            */
-        png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING,	NULL, NULL, NULL);
-
-        if(png_ptr == NULL)
-        {
-            gef::DebugOut("Could not create png struct: %s", filename);
-            return;
-        }
-
-        /* Allocate/initialize the memory
-            * for image information.  REQUIRED. */
-        info_ptr = png_create_info_struct(png_ptr);
-        if (info_ptr == NULL)
-        {
-            png_destroy_read_struct(&png_ptr, NULL, NULL);
-            return;
-        }
-        
-
-		PNGData data;
-		data.p = buffer.data();
-		data.len = buffer.size();
-
-        png_set_read_fn(png_ptr, &data, ReadDataFromInputStream);
-
-        /* If we have already
-            * read some of the signature */
-        png_set_sig_bytes(png_ptr, sig_read);
-
-        png_read_info(png_ptr, info_ptr);
-
-        png_uint_32 retval = png_get_IHDR(png_ptr, info_ptr,
-                &width,
-                &height,
-                &bitDepth,
-                &colorType,
-                NULL, NULL, NULL);
-
-        if(retval != 1)
-        {
-            /* Free all of the memory associated
-                * with the png_ptr and info_ptr */
-            png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
-            return;
-        }
-        
-
-        if (colorType == PNG_COLOR_TYPE_RGB)
-        {
-            png_set_add_alpha(png_ptr, 0xff, PNG_FILLER_AFTER);
-            png_read_update_info(png_ptr, info_ptr);
-
-            png_uint_32 retval = png_get_IHDR(png_ptr, info_ptr,
-                &width,
-                &height,
-                &bitDepth,
-                &colorType,
-                NULL, NULL, NULL);
-
-            if (retval != 1)
+            // read entire file
+            UInt8* buffer = NULL;
+            Int32 file_size;
+            success = png_file->GetSize(file_size);
+            if(success)
             {
-                /* Free all of the memory associated
-                    * with the png_ptr and info_ptr */
-                png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
-                return;
+                buffer = new UInt8[file_size];
+                Int32 bytes_read;
+                success = png_file->Read(buffer, file_size, bytes_read);
+
+                if(success)
+                    success = png_file->Close();
+
+                if(success)
+                {
+                    png_structp png_ptr;
+                    png_infop info_ptr;
+                    UInt32 sig_read = 0;
+                    png_uint_32 width = 0;
+                    png_uint_32 height = 0;
+                    int bitDepth = 0;
+                    int colorType = -1;
+
+
+                    /* Create and initialize the png_struct
+                     * with the desired error handler
+                     * functions.  If you want to use the
+                     * default stderr and longjump method,
+                     * you can supply NULL for the last
+                     * three parameters.  We also supply the
+                     * the compiler header file version, so
+                     * that we know if the application
+                     * was compiled with a compatible version
+                     * of the library.  REQUIRED
+                     */
+                    png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING,	NULL, NULL, NULL);
+
+                    if(png_ptr == NULL)
+                    {
+                        success = false;
+                        png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+                        delete[] buffer;
+                        buffer = NULL;
+                    }
+
+                    if(success)
+                    {
+                        /* Allocate/initialize the memory
+                         * for image information.  REQUIRED. */
+                        info_ptr = png_create_info_struct(png_ptr);
+                        if (info_ptr == NULL)
+                        {
+                            success = false;
+                            delete[] buffer;
+                            buffer = NULL;
+                            png_destroy_read_struct(&png_ptr, NULL, NULL);
+                        }
+                    }
+
+                    if(success)
+                    {
+                        /* Set error handling if you are
+                         * using the setjmp/longjmp method
+                         * (this is the normal method of
+                         * doing things with libpng).
+                         * REQUIRED unless you  set up
+                         * your own error handlers in
+                         * the png_create_read_struct()
+                         * earlier.
+                         */
+                        if (setjmp(png_jmpbuf(png_ptr)))
+                        {
+                            success = false;
+                            delete[] buffer;
+                            buffer = NULL;
+
+                            /* Free all of the memory associated
+                             * with the png_ptr and info_ptr */
+                            png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+                        }
+                    }
+
+                    if(success)
+                    {
+						PNGData data;
+						data.p = buffer;
+						data.len = file_size;
+
+                        png_set_read_fn(png_ptr, &data, ReadDataFromInputStream);
+
+                        /* If we have already
+                         * read some of the signature */
+                        png_set_sig_bytes(png_ptr, sig_read);
+
+                        png_read_info(png_ptr, info_ptr);
+
+                        png_uint_32 retval = png_get_IHDR(png_ptr, info_ptr,
+                                &width,
+                                &height,
+                                &bitDepth,
+                                &colorType,
+                                NULL, NULL, NULL);
+
+                        if(retval != 1)
+                        {
+                            success = false;
+                            delete[] buffer;
+                            buffer = NULL;
+
+                            /* Free all of the memory associated
+                             * with the png_ptr and info_ptr */
+                            png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+                        }
+                    }
+
+                    if(success)
+                    {
+                        if(colorType == PNG_COLOR_TYPE_RGB)
+                        {
+                            png_set_add_alpha(png_ptr, 0xff, PNG_FILLER_AFTER);
+                            png_read_update_info(png_ptr, info_ptr);
+
+                            png_uint_32 retval = png_get_IHDR(png_ptr, info_ptr,
+                                    &width,
+                                    &height,
+                                    &bitDepth,
+                                    &colorType,
+                                    NULL, NULL, NULL);
+
+                            if(retval != 1)
+                            {
+                                success = false;
+                                delete[] buffer;
+                                buffer = NULL;
+
+                                /* Free all of the memory associated
+                                 * with the png_ptr and info_ptr */
+                                png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+                            }
+
+                        }
+                    }
+
+                    if(success)
+                    {
+                        image_data.set_width(width);
+                        image_data.set_height(height);
+
+						if (colorType == PNG_COLOR_TYPE_RGB_ALPHA)
+						{
+							UInt32 row_bytes = (UInt32)png_get_rowbytes(png_ptr, info_ptr);
+							UInt8* image_bytes = (UInt8*)malloc(row_bytes * height);
+							image_data.set_image(image_bytes);
+						}
+
+
+                        switch(colorType)
+                        {
+                            case PNG_COLOR_TYPE_RGB:
+                                //      ParseRGB(outImage, png_ptr, info_ptr);
+                                break;
+
+                            case PNG_COLOR_TYPE_RGB_ALPHA:
+                                //outHasAlpha = true;
+                                ParseRGBA(image_data.image(), png_ptr, info_ptr, width, height);
+                                break;
+
+                        }
+
+						/* Free all of the memory associated
+						* with the png_ptr and info_ptr */
+						png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+                    }
+                }
+
+                // done
+                delete[] buffer;
+                buffer = NULL;
             }
-
         }
-
-        image_data.set_width(width);
-        image_data.set_height(height);
-
-        if (colorType == PNG_COLOR_TYPE_RGB_ALPHA)
-        {
-            UInt32 row_bytes = (UInt32)png_get_rowbytes(png_ptr, info_ptr);
-            UInt8* image_bytes = (UInt8*)malloc(row_bytes * height);
-            image_data.set_image(image_bytes);
-        }
-
-
-        switch (colorType)
-        {
-        case PNG_COLOR_TYPE_RGB:
-            ParseRGB(image_data.image(), png_ptr, info_ptr, width, height);
-            break;
-
-        case PNG_COLOR_TYPE_RGB_ALPHA:
-            //outHasAlpha = true;
-            ParseRGBA(image_data.image(), png_ptr, info_ptr, width, height);
-            break;
-
-        }
-
-        /* Free all of the memory associated
-        * with the png_ptr and info_ptr */
-        png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
     }
 
-    void PNGLoader::ParseRGBA(UInt8* out_image_buffer, png_structp the_png_ptr, const png_infop the_info_ptr, UInt32 width, UInt32 height)
+    void PNGLoader::ParseRGBA(UInt8* out_image_buffer, void* the_png_ptr,
+            const void* the_info_ptr, UInt32 width, UInt32 height)
     {
         png_structp png_ptr = (png_structp)the_png_ptr;
         png_infop info_ptr = (png_infop)the_info_ptr;
@@ -227,29 +256,6 @@ namespace gef
         delete [] rowData;
 
     }  // end ParseRGBA()
-
-    void PNGLoader::ParseRGBA(UInt8* out_image_buffer, void* the_png_ptr,
-        const void* the_info_ptr, UInt32 width, UInt32 height)
-    {
-        png_structp png_ptr = (png_structp)the_png_ptr;
-        png_infop info_ptr = (png_infop)the_info_ptr;
-
-        const png_uint_32 bytesPerRow = (png_uint_32)png_get_rowbytes(png_ptr, info_ptr);
-        UInt8* rowData = new UInt8[bytesPerRow];
-        const UInt32 kBytesPerPixel = bytesPerRow / width;
-
-        // read single row at a time
-        for (UInt32 rowIdx = 0; rowIdx < height; ++rowIdx)
-        {
-            png_read_row(png_ptr, (png_bytep)rowData, NULL);
-
-            const UInt32 rowOffset = rowIdx * width;
-            UInt32 pixel_byte_offset = rowOffset * kBytesPerPixel;
-            memcpy(&out_image_buffer[pixel_byte_offset], rowData, width * kBytesPerPixel);
-        }
-
-        delete[] rowData;
-    }
 
     UInt32 PNGLoader::NextPowerOfTwo(const UInt32 value)
     {
--- assets/png_loader.h
+++ assets/png_loader.h
@@ -3,8 +3,6 @@
 
 #include <gef.h>
 
-class png_info;
-class png_struct;
 
 namespace gef
 {
@@ -20,8 +18,7 @@ namespace gef
 
 		void Load(const char* filename, const Platform& platform, ImageData& image_data);
 	private:
-		void ParseRGB(UInt8* out_image_buffer, png_struct* the_png_ptr, const png_info* the_info_ptr, UInt32 width, UInt32 height);
-		void ParseRGBA(UInt8* out_image_buffer, png_struct* the_png_ptr, const png_info* the_info_ptr, UInt32 width, UInt32 height);
+		void ParseRGBA(UInt8* out_image_buffer, void* png_ptr, const void* info_ptr, UInt32 width, UInt32 height);
 		UInt32 NextPowerOfTwo(const UInt32 value);
 	};
 
