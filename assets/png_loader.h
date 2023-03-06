c541eaa69b4fa73917fcb978eca686a4f4b32cee
2100719 <2100719@uad.ac.uk>
2023-03-06 11:14:31 +0000

    temp

--- assets/png_loader.h
+++ assets/png_loader.h
@@ -3,6 +3,8 @@
 
 #include <gef.h>
 
+class png_info;
+class png_struct;
 
 namespace gef
 {
@@ -18,7 +20,8 @@ namespace gef
 
 		void Load(const char* filename, const Platform& platform, ImageData& image_data);
 	private:
-		void ParseRGBA(UInt8* out_image_buffer, void* png_ptr, const void* info_ptr, UInt32 width, UInt32 height);
+		void ParseRGB(UInt8* out_image_buffer, png_struct* the_png_ptr, const png_info* the_info_ptr, UInt32 width, UInt32 height);
+		void ParseRGBA(UInt8* out_image_buffer, png_struct* the_png_ptr, const png_info* the_info_ptr, UInt32 width, UInt32 height);
 		UInt32 NextPowerOfTwo(const UInt32 value);
 	};
 
