2bd3f41a5ab5d4866093678a095b3c0dd936d433
FinlaySML <2100719@uad.ac.uk>
2023-03-29 17:09:23 +0100

    Change sprite shader to use no filtering so that the icons are *crisp*

--- graphics/default_sprite_shader.cpp
+++ graphics/default_sprite_shader.cpp
@@ -58,7 +58,7 @@ namespace gef
 
 		// Create a texture sampler state description.
 		D3D11_SAMPLER_DESC sampler_desc;
-		sampler_desc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+		sampler_desc.Filter = D3D11_FILTER_MIN_MAG_MIP_POINT;//D3D11_FILTER_MIN_MAG_MIP_LINEAR;
 		sampler_desc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
 		sampler_desc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
 		sampler_desc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
